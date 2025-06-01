d2d50154274366f1c7b125d026fc624b467c900f
shivammittal <devoteeofbadrinath@ymail.com>
2025-06-01 14:39:09 +0530

    Message

---
+++ section1/brdjstore/src/test/java/com/brdjdob/brdjstore/BrdjstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brdjdob.brdjstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrdjstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
