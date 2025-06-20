e361174a6de789da58f476f137a110f45c33c641
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-06-20 20:36:26 +0530

    Message

--- section3/brdjstore-ui/src/App.jsx
+++ section3/brdjstore-ui/src/App.jsx
@@ -1,6 +1,18 @@
-import './App.css'
+import Header from "./components/Header"
+import ClassHeader from "./components/ClassHeader"; 
+import Footer from "./components/footer/Footer";
+import React from "react";
+import Home from "./components/Home";
 
 function App() {
+    return (
+        <>
+            <Header/>
+            <Home/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App
+ 
