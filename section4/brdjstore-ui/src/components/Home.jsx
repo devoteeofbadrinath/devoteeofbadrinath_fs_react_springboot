a7bfe69a521d909076815afa04b7ed38889b63da
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-06-25 14:34:29 +0530

    Message

--- section4/brdjstore-ui/src/components/Home.jsx
+++ section4/brdjstore-ui/src/components/Home.jsx
@@ -3,9 +3,17 @@ import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 
 export default function Home() {
+    const isActive = Math.random() > 0.5;
+
+    const h1Styling = {
+    textAlign: "center",
+    color: isActive? "black":"red"
+    }
+
     return (
         <>
-            <h1 className="my-heading">Demo of Global CSS Scope from Home</h1>
+            <h1 style={h1Styling}>Demo of Global CSS Scope from Home</h1>
+            {/* <h1 className="my-heading">Demo of Global CSS Scope from Home</h1> */}
             <div className="home-container">
             <PageHeading title="Explore Brdj stickers">
             Add a touch of creativity to your space with our wide range of fun and
