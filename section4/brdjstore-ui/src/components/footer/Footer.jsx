9ef0eb15f673b735c7db039c456bb5252adfd169
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-07-04 18:11:56 +0530

    Message

--- section4/brdjstore-ui/src/components/footer/Footer.jsx
+++ section4/brdjstore-ui/src/components/footer/Footer.jsx
@@ -2,18 +2,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 // import "./footer.css";
 import styles from "./footer.module.css";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+color: #5b21b6;
+text-align: center;
+`;
 
 export default function Footer(){
     const isActive = Math.random() > 0.5;
     return (
         <>
-            <h1 
+            <H1>Demo of Styled Components from Footer</H1>
+            {/* <h1 
                 className={`${styles["my-heading"]} ${
                 isActive ? styles["primary-color"] : styles["secondary-color"]
                 }`}
             >
                 Demo of Global CSS Scope from Home
-            </h1>
+            </h1> */}
             {/* <h1 
             style={{
                 textAlign: 'center', 
