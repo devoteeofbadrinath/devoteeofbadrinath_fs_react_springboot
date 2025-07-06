1f4ab12d7a68d431b77a75bb92687a88670aad24
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-07-06 19:02:38 +0530

    Message

--- section4/brdjstore-ui/src/components/BrdjButton.js
+++ section4/brdjstore-ui/src/components/BrdjButton.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const BrdjButton = styled.button`
-background-color:${(props) = (props.primary ? "#5b21b6" : "black")}
+background-color:${(props) => (props.$primary ? "#5b21b6" : "black")};
 color: white;
 padding: 10px 20px;
 border: none;
@@ -9,6 +9,14 @@ border-radius: 5px;
 cursor: pointer;
 display: block;
 margin: 0 auto;
-`
+
+&:hover {
+background-color: blue;
+}
+
+&:focus {
+outline: 2px solid white;
+}
+`;
 
 export default BrdjButton
