2addf6489bd735d7c12abe93ab239e07e5a125a7
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-07-10 22:22:26 +0530

    Message

--- section4/brdjstore-ui/src/components/Home.jsx
+++ section4/brdjstore-ui/src/components/Home.jsx
@@ -64,6 +64,34 @@ export default function Home() {
                     ></button>
                 </div>
             </div>
+            <div className="container d-flex justify-content-center align-items-center">
+            <div className="card" style={{width: "18rem"}}>
+            <img src={developerImage} className="card-img-top" alt="..."/>    
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the 
+                    bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                    Go somewhere
+                </a>
+            </div>
+            </div>
+            <div className="card" style={{width: "18rem"}}>
+            <img src={breakImage} className="card-img-top" alt="..."/>    
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the 
+                    bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                    Go somewhere
+                </a>
+            </div>
+            </div>
+            </div>
             <PageHeading title="Explore Brdj stickers">
             Add a touch of creativity to your space with our wide range of fun and
             unique stickers. Perfect for any occasion!
