35ab9fbdd6606ed6d795c0b4b7536ef4a5407117
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-07-14 13:19:58 +0530

    Message

--- section5/tailwind-demo/src/App.jsx
+++ section5/tailwind-demo/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 function App() {
   return (
   <>
-    <h1 className='text-3xl font-bold underline'>Hello world!</h1>  
+    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <p className='text-lg underline font-bold mt-4 text-blue-600'>Tailwind makes styling easy</p>
   </>
   );
 }
