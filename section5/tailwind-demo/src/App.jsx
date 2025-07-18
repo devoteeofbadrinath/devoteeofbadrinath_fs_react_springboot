c519ca499eb01f8740c0f59303709b1385d5cc4a
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-07-18 23:40:20 +0530

    Message

--- section5/tailwind-demo/src/App.jsx
+++ section5/tailwind-demo/src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 function App() {
   return (
   <>
-    {/* <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     <p className='text-lg underline font-bold mt-4 text-blue-600'>
       Tailwind makes styling easy
     </p>
   <div className='bg-gray-200 p-4 m-4'>Background color demo</div>
-  <div className='bg-green-300 border border-green-600 p-4 rounded-lg'>
+  <div className='bg-green-300 border border-green-600 p-4 rounded-lg m-4'>
     Border demo
   </div>
   <div className='grid grid-cols-3 gap-4 mt-4'>
@@ -82,20 +82,28 @@ function App() {
     >
       Read More
       </button>
-  </div> */}
-  <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+  </div>
+  <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4'>
     <div className='bg-blue-300 p-4'>1</div>
     <div className='bg-blue-300 p-4'>2</div>
     <div className='bg-blue-300 p-4'>3</div>
     <div className='bg-blue-300 p-4'>4</div>
   </div>
        <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-4xl m-4">
-        
-        I scale on different screens!
+       I scale on different screens!
       </p>
       <p className="text-red-600 sm:text-blue-600 md:text-emerald-600 lg:text-yellow-500 xl:text-purple-600 m-4">
         Resize the screen to see changes!
-      </p> 
+      </p>
+  <div className='bg-[#ff5733] h-[165px] w-[300px] border border-green-600 p-4 rounded-lg m-4 flex items-center justify-center'>
+    Custom Width, Height and Color
+  </div>
+  <div className='bg-shivam-500 h-[165px] w-[300px] border border-green-600 p-4 rounded-lg m-4 flex items-center justify-center'>
+    Theme variables
+  </div>
+  <div className='bg-primary font-display h-[165px] w-[300px] border border-green-600 p-4 rounded-lg m-4 flex items-center justify-center'>
+    Font Family and Color Variables
+  </div>
   </>
   );
 }
