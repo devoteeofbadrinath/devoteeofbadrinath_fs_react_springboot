7055d7d1ddd330d3d2b03218484329112ba85a25
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-07-18 23:40:44 +0530

    Message

---
+++ section5/brdjstore-ui/src/components/PageTitle.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Children } from 'react'
+
+export default function PageTitle({title, children}) {
+    return (
+    <h1 className='text-3xl font-primary font-extrabold text-center text-primary mt-4 py-2'>{title}</h1>
+  ) 
+}
