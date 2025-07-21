70368bcc9d05f620b7bf71a88f9af77fe28e859e
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-07-21 21:16:02 +0530

    Message

---
+++ section6/coreconcepts/src/main/java/com/brdjdob/coreconcepts/BoseSpeaker.java
@@ -0,0 +1,9 @@
+package com.brdjdob.coreconcepts;
+
+public class BoseSpeaker implements Speaker {
+
+    @Override
+    public String makeSound() {
+        return "Sound from Bose Speakers";
+    }
+}
