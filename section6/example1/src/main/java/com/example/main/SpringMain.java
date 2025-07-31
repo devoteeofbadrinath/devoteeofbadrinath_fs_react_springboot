341a8ef19fac9af9757cf3f339e708cdf781cd1e
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-07-31 23:06:05 +0530

    Message

--- section6/example1/src/main/java/com/example/main/SpringMain.java
+++ section6/example1/src/main/java/com/example/main/SpringMain.java
@@ -6,6 +6,12 @@ import com.example.config.ProjectConfig;
 public class SpringMain {
 
     public static void main(String[] args) {
+
+        /*
+        The var keyword was introduced in Java 10. Type inference is used in
+        var keyword in which it detects automatically the datatype of a variable
+        based on the surrounding context.
+         */
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
     }
