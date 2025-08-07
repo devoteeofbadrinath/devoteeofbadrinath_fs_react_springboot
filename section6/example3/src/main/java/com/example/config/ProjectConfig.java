eecb5a94d78139e185b990fd0bc5e9e9886ce063
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-08-07 22:27:18 +0530

    Message

--- section6/example3/src/main/java/com/example/config/ProjectConfig.java
+++ section6/example3/src/main/java/com/example/config/ProjectConfig.java
@@ -3,7 +3,6 @@ package com.example.config;
 import com.example.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 
 @Configuration
@@ -24,14 +23,6 @@ public class ProjectConfig {
         return vehicle;
     }
 
-    /*
-    When you have multiple beans of the same kind inside the Spring context.
-    you can make one of them primary by using @Primary annotation. Primary bean is
-    the one which Spring will choose if it has multiple options and you don't specify a name.
-    In other words, it is the default bean that Spring Context will consider in case of
-    confusion due to multiple beans present of same type.
-     */
-    @Primary
     @Bean("ferrariVehicle")
     Vehicle vehicle3() {
         Vehicle vehicle = new Vehicle();
