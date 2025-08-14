cf8473c10a322f2c56bac3f5ac8f58bcf3867f20
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-08-14 11:32:35 +0530

    Message

---
+++ section6/example5/src/main/java/com/example/main/SpringMain.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.example.config.ProjectConfig;
+
+public class SpringMain {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Component Vehicle name from Spring Context is: " + vehicle.getName());
+        vehicle.sayHello();
+    }
+
+}
