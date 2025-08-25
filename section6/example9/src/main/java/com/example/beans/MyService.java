b2efa011647fadc6c27497b9705a875708146f2b
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-08-25 21:19:28 +0530

    Message

---
+++ section6/example9/src/main/java/com/example/beans/MyService.java
@@ -0,0 +1,14 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(BeanDefinition.SCOPE_SINGLETON) // Optional
+public class MyService {
+
+    public MyService() {
+        System.out.println("My Service created");
+    }
+}
