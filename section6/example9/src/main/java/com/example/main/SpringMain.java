37b6020b444829da723923528c1939c57a76172e
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-08-27 20:23:40 +0530

    Message

--- section6/example9/src/main/java/com/example/main/SpringMain.java
+++ section6/example9/src/main/java/com/example/main/SpringMain.java
@@ -1,6 +1,7 @@
 package com.example.main;
 
 import com.example.beans.MyService;
+import com.example.beans.UserSession;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import com.example.config.ProjectConfig;
 
@@ -8,10 +9,16 @@ public class SpringMain {
 
     public static void main(String[] args) {
 
+//        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+//        MyService service1 = context.getBean(MyService.class);
+//        MyService service2 = context.getBean(MyService.class);
+
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        MyService service1 = context.getBean(MyService.class);
-        MyService service2 = context.getBean(MyService.class);
-        System.out.println(service1 == service2); // Comparing hash code
+        UserSession user1 = context.getBean(UserSession.class);
+        UserSession user2 = context.getBean(UserSession.class);
+        System.out.println(user1 == user2); // Comparing hash code
+        System.out.println(user1.getSessionId());
+        System.out.println(user2.getSessionId());
     }
 
 }
