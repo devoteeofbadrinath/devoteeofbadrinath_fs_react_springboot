997db6f062f7af2449446bca94448555eee6ce67
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-08-28 23:42:20 +0530

    Message

---
+++ section7/brdjstore/src/main/java/com/brdjdob/brdjstore/BrdjstoreApplication.java
@@ -0,0 +1,13 @@
+package com.brdjdob.brdjstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrdjstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrdjstoreApplication.class, args);
+	}
+
+}
