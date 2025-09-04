cf32c5ba7206b9f72b9c19021c0188a5b9177230
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-09-04 22:27:51 +0530

    Message

--- section7/brdjstore/src/main/java/com/brdjdob/brdjstore/BrdjstoreApplication.java
+++ section7/brdjstore/src/main/java/com/brdjdob/brdjstore/BrdjstoreApplication.java
@@ -3,9 +3,11 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
- @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class BrdjstoreApplication {
+ @SpringBootApplication
+//@ComponentScan(basePackages = {"com.brdjdob.brdjstore.controller"})
+ public class BrdjstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrdjstoreApplication.class, args);
