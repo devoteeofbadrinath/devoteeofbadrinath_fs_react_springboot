39086792cdcd090cba0dee2a7cfdf410013481f2
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-09-09 19:35:14 +0530

    Message

--- section7/brdjstore/src/main/java/com/brdjdob/brdjstore/controller/ProductController.java
+++ section7/brdjstore/src/main/java/com/brdjdob/brdjstore/controller/ProductController.java
@@ -1,16 +1,26 @@
 package com.brdjdob.brdjstore.controller;
 
+import com.brdjdob.brdjstore.entity.Product;
+import com.brdjdob.brdjstore.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductRepository productRepository;
+
     @GetMapping
-    public String getProducts() {
-        return "Here are your products";
+    public List<Product> getProducts() {
+        List<Product> productList = productRepository.findAll();
+        return productList;
     }
 
 }
