ea317445b70164291c8b88e88cde72addf93d5e3
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-09-11 22:42:00 +0530

    Message

---
+++ section7/brdjstore/src/main/java/com/brdjdob/brdjstore/service/IProductService.java
@@ -0,0 +1,9 @@
+package com.brdjdob.brdjstore.service;
+
+import com.brdjdob.brdjstore.entity.Product;
+import java.util.List;
+
+public interface IProductService {
+
+    List<Product> getProducts();
+}
